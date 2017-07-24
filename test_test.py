ddb4083b6c4fbd5147c7e8e1e1c42366de6d49c9
Eugene Kravchenko <evgeniy.kravchenko@oracle.com>
2017-07-24 16:23:02 +0300

    find interested domains

--- test_test.py
+++ test_test.py
@@ -16,23 +16,23 @@ class SimpleDomainTestCase(unittest.TestCase):
         self.last_page = 0
         self.matches = None
 
-    # def test_collect(self, page=2):
-    #     self.wait.until_not(EC.visibility_of_element_located((By.ID, 'domtable_processing')))
-    #     domains = self.driver.find_elements_by_css_selector('#tablewrap td:nth-child(2)')
-    #     for domain in domains:
-    #         self.domain_list.append(domain.text)
-    #     sql.add_new_domain(self.domain_list)
-    #
-    #     next_pages = self.driver.find_elements_by_css_selector('#domtable_paginate > span > a')
-    #     for next_page in next_pages:
-    #         if int(next_page.text) == page:
-    #             self.last_page = next_page.text
-    #             next_page.click()
-    #             self.domain_list = []
-    #             break
-    #     if int(self.last_page) < page:
-    #         return
-    #     self.test_collect(page + 1)
+    def test_collect(self, page=2):
+        self.wait.until_not(EC.visibility_of_element_located((By.ID, 'domtable_processing')))
+        domains = self.driver.find_elements_by_css_selector('#tablewrap td:nth-child(2)')
+        for domain in domains:
+            self.domain_list.append(domain.text)
+        sql.add_new_domain(self.domain_list)
+
+        next_pages = self.driver.find_elements_by_css_selector('#domtable_paginate > span > a')
+        for next_page in next_pages:
+            if int(next_page.text) == page:
+                self.last_page = next_page.text
+                next_page.click()
+                self.domain_list = []
+                break
+        if int(self.last_page) < page:
+            return
+        self.test_collect(page + 1)
 
     def test_find_white_domains(self):
         self.matches = sql.get_domains_by_name('20')
